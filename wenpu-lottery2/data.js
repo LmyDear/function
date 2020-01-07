3c0d73273da083fef1d0358754aeeb182bd2be0c
LmyDear <38447336+LmyDear@users.noreply.github.com>
2020-01-07 10:15:04 +0800

    3d动画抽奖

---
+++ wenpu-lottery2/data.js
@@ -0,0 +1,204 @@
+var data=[
+    {
+        "nameen": "avatar1",
+        "namezh": "刘鑫慈",
+        "phone":"133****4630",
+        "wish": "新年快乐，们委的交型关确音几心她民知历解。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar2",
+        "namezh": "马力",
+        "phone":"135****3041",
+        "wish": "新年快乐，龙公平儿叫许车际展去状第论。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar3",
+        "namezh": "宋彦涛",
+        "phone":"139****2016",
+        "wish": "新年快乐，十小叫社法革领事因水务住酸事义小。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar4",
+        "namezh": "赵志强",
+        "phone":"139****0632",
+        "wish": "新年快乐，得表新传目物九切设清志在油几口名调一再。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar5",
+        "namezh": "罗广超",
+        "phone":"189****2287",
+        "wish": "新年快乐，片土列们准军看上调划准常员原进。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar6",
+        "namezh": "朱国栋",
+        "phone":"136****8949",
+        "wish": "新年快乐，九论期要着音区内公观积。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar7",
+        "namezh": "王雨",
+        "phone":"183****1571",
+        "wish": "新年快乐，了教公没基改特低体细角好无二联片里都系。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar8",
+        "namezh": "盛祥（华腾）",
+        "phone":"133****9990",
+        "wish": "新年快乐，理由正用点美最属持象低集性期部条。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar9",
+        "namezh": "赵莲花",
+        "phone":"153****5882",
+        "wish": "新年快乐，几法完般石度成资位今派至林转面则改。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar10",
+        "namezh": "周楷钧",
+        "phone":"186****8393",
+        "wish": "新年快乐，西使命带值直分把史达信且表计管海。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar11",
+        "namezh": "樊燕芳",
+        "phone":"186****1536",
+        "wish": "新年快乐，两从元处经争复当场导规起规织之组。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar12",
+        "namezh": "石蕊",
+        "phone":"138****1780",
+        "wish": "新年快乐，况机比来十回是算军传方照先想。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar13",
+        "namezh": "郑振宇",
+        "phone":"189****3039",
+        "wish": "新年快乐，土及例育此求运大立价标点。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar15",
+        "namezh": "穆永春（华腾）",
+        "phone":"183****5855",
+        "wish": "新年快乐，划完面克用规五看八下地什群金下许公条话。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar16",
+        "namezh": "黄正宇（大宇）",
+        "phone":"137****4939",
+        "wish": "新年快乐，前走式调细确林资易山把。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar17",
+        "namezh": "张晓东（华腾）",
+        "phone":"177****6541",
+        "wish": "新年快乐，线很持装布二教想新马九土际料眼分第精。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar18",
+        "namezh": "唐保林",
+        "phone":"151****3090",
+        "wish": "新年快乐，本件什有量今据收导我那无文。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar19",
+        "namezh": "孙余飞",
+        "phone":"152****3457",
+        "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar20",
+        "namezh": "陈弢",
+        "phone":"138****2319",
+        "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar21",
+        "namezh": "冯景瑶",
+        "phone":"176****3005",
+        "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar22",
+        "namezh": "张家椋",
+        "phone":"153****8802",
+        "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar23",
+        "namezh": "张寅",
+        "phone":"185****8717",
+        "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar24",
+        "namezh": "汪文龙",
+        "phone":"182****4428",
+        "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar25",
+        "namezh": "吴晓峰",
+        "phone":"135****3421",
+        "wish": "新年快乐，收般得可史常办现他把业千严么史广当说用。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar26",
+        "namezh": "郭勇男",
+        "phone":"185****9836",
+        "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。",
+        "namedep": "易惠宝"
+        },
+        {
+        "nameen": "avatar27",
+        "namezh": "张萍（华腾）",
+        "phone":"135****2700",
+        "wish": "新年快乐，cas按上次的事v辅导班地方。",
+        "namedep": "易惠宝"
+        },
+        // {
+        // "nameen": "avatar28",
+        // "namezh": "林文荣",
+        // "phone":"133****1366",
+        // "wish": "新年快乐，首次发布通告还能地方v人突然被人诟病。",
+        // "namedep": "易惠宝"
+        // },
+        // {
+        // "nameen": "avatar29",
+        // "namezh": "严敏",
+        // "phone":"185****5810",
+        // "wish": "新年快乐，的发布会他也会能否给个奴役贴吧",
+        // "namedep": "易惠宝"
+        // }
+            
+]
+// 初始化数据
+if (!localStorage.getItem('lottery_initial')) {
+    var data_str = JSON.stringify(data);
+    localStorage.setItem('lottery_initial', data_str);
+}
